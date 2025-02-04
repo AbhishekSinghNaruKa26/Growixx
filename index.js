db2880c93e28103d5b2bb92c73a4c8e039fd1424
Abhishek Singh Naruka <abhisheksingh200326@gmail.com>
2025-02-05 01:29:40 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -9,11 +9,46 @@ const scroll=ScrollReveal(
         distance:'60px'
     })
     scroll.reveal('.text-transfer',{
-        delay:400,
+        delay:600,
         origin:'bottom'
     })
 
     scroll.reveal('.text2',{
-        delay:400,
-        origin:''
+        delay:900,
+        origin:'bottom'
     })
+
+
+    let section= document.querySelectorAll('section')
+    let navlink= document.querySelectorAll('a header nav ')
+    window.onscroll=()=>{
+        section.forEach(sec=>{
+            let top=window.scrollY;
+            let offset=sec.offsetTop-150;
+            let height=sec.offsetHeight;
+            let id=sec.getAttribute('id');
+            if(top>offset && top<offset + height){
+                navlink.forEach(links=>{
+                    links.classList.remove('active')
+                    document.querySelector('a[href*='+id+'] header nav ').classList.add('active')
+                })
+            }
+        })
+    }
+
+
+    let valueDisplays= document.querySelectorAll(".num");
+    let interval= 1000;
+
+    valueDisplays.forEach((valueDisplays)=>{
+        let startValue= 0;
+        let endValue= parseInt(valueDisplays.getAttribute("data-val"));
+        let duration=Math.floor(interval / endValue);
+        let counter = setInterval(function (){
+            startValue += 1;
+            valueDisplays.textContent = startValue;
+            if(startValue == endValue){
+                clearInterval(counter);
+            }
+        },duration);
+    });
